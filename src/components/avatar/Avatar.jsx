583d388e97b300242206a940d89cbf8aaf461a5a
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-17 15:50:00 +0200

    feat: add Avatar component

---
+++ src/components/avatar/Avatar.jsx
@@ -0,0 +1,23 @@
+import classNames from "classnames";
+import styles from "./Avatar.module.scss";
+
+export default function Avatar({
+  size = "90",
+  url = "/images/avatar.png",
+  verified = true,
+}) {
+  return (
+    <div
+      style={{ width: size + "px", height: size + "px" }}
+      className={classNames(styles.avatar)}
+    >
+      <img src={url} alt="" className={classNames(styles.image)} />
+      {verified && (
+        <img
+          src="/images/verified.svg"
+          className={classNames(styles.badge)}
+        />
+      )}
+    </div>
+  );
+}
