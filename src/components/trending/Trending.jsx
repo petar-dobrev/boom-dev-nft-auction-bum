aa6b26217a0fd6ca9ac13a02120a0cbc0a8baaa9
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-19 11:02:11 +0200

    feat: add title and select components

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -1,13 +1,105 @@
-import { Grid, Container } from "@mui/material";
+import { useState } from "react";
+
+import {
+  Grid,
+  Container,
+  Typography,
+  FormControl,
+  Select,
+  InputLabel,
+  MenuItem,
+} from "@mui/material";
 import Card from "../card/Card";
 
 import styles from "./Trending.module.scss";
 import classNames from "classnames";
 
-export default function Trending({ cards = [] }) {
+export default function Trending({
+  cards = [
+    {
+      name: "Ivy",
+      user: {
+        avatar: {
+          url: "images/avatar.png",
+        },
+        verified: true,
+      },
+      mediaUrl: "images/nft.jpg",
+      price: 1,
+      currency: "ETH",
+    },
+    {
+      name: "Judie",
+      user: {
+        avatar: {
+          url: "images/avatar.png",
+        },
+        verified: true,
+      },
+      mediaUrl: "images/nft.jpg",
+      price: 2.3,
+      currency: "ETH",
+    },
+    {
+      name: "Juniper",
+      user: {
+        avatar: {
+          url: "images/avatar.png",
+        },
+        verified: true,
+      },
+      mediaUrl: "images/nft.jpg",
+      price: 5,
+      currency: "ETH",
+    },
+    {
+      name: "Maple",
+      user: {
+        avatar: {
+          url: "images/avatar.png",
+        },
+        verified: true,
+      },
+      mediaUrl: "images/nft.jpg",
+      price: 10,
+      currency: "ETH",
+    },
+  ],
+}) {
+  const [selectedDateRange, setSelectedDateRange] = useState("This week");
+
+  const handleChange = (event) => {
+    setSelectedDateRange(event.target.value);
+  };
+
   return (
     <Container className={classNames(styles.container)} maxWidth="xl">
-      <Grid container spacing={2}>
+      <Grid
+        container
+        alignContent="center"
+        justifyContent="space-between"
+        sx={{ my: 2 }}
+      >
+        <Grid item>
+          <Typography variant="h2">Trending</Typography>
+        </Grid>
+        <Grid item>
+          <FormControl sx={{ minWidth: 120 }}>
+            <Select
+              variant="outlined"
+              value={selectedDateRange}
+              onChange={handleChange}
+              inputProps={{ "aria-label": "Without label" }}
+            >
+              <MenuItem value="This week">This week</MenuItem>
+              <MenuItem value="This month">This month</MenuItem>
+              <MenuItem value="Last week">Last week</MenuItem>
+              <MenuItem value="Last month">Last month</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} justifyContent="space-between">
         {cards.map((card) => (
           <Grid item key={Math.floor(Math.random() * Date.now()).toString(16)}>
             <Card
