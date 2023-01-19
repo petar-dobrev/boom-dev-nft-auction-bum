21bd51ff9fd4ca9233d937829b7c504577eee01d
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-19 11:13:56 +0200

    fix: removed dummy testing data

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -14,58 +14,7 @@ import Card from "../card/Card";
 import styles from "./Trending.module.scss";
 import classNames from "classnames";
 
-export default function Trending({
-  cards = [
-    {
-      name: "Ivy",
-      user: {
-        avatar: {
-          url: "images/avatar.png",
-        },
-        verified: true,
-      },
-      mediaUrl: "images/nft.jpg",
-      price: 1,
-      currency: "ETH",
-    },
-    {
-      name: "Judie",
-      user: {
-        avatar: {
-          url: "images/avatar.png",
-        },
-        verified: true,
-      },
-      mediaUrl: "images/nft.jpg",
-      price: 2.3,
-      currency: "ETH",
-    },
-    {
-      name: "Juniper",
-      user: {
-        avatar: {
-          url: "images/avatar.png",
-        },
-        verified: true,
-      },
-      mediaUrl: "images/nft.jpg",
-      price: 5,
-      currency: "ETH",
-    },
-    {
-      name: "Maple",
-      user: {
-        avatar: {
-          url: "images/avatar.png",
-        },
-        verified: true,
-      },
-      mediaUrl: "images/nft.jpg",
-      price: 10,
-      currency: "ETH",
-    },
-  ],
-}) {
+export default function Trending({ cards = [] }) {
   const [selectedDateRange, setSelectedDateRange] = useState("This week");
 
   const handleChange = (event) => {
