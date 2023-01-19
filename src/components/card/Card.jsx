4c177de499b8936aa37862688f5553afecc75d16
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-19 17:17:31 +0200

    feat: add live state to the component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,6 +4,8 @@ import millify from "millify";
 
 import { Card as MuiCard, Chip, CardContent, Typography } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import CircleIcon from "@mui/icons-material/Circle";
+import Countdown, { zeroPad } from "react-countdown";
 import Avatar from "../avatar/Avatar";
 
 import styles from "./Card.module.scss";
@@ -18,18 +20,48 @@ export default function Card(props) {
     user = { avatar: { url: "" }, verified: false },
     price = "",
     currency = "",
+    timeLeft,
   } = props;
 
   const handleClick = () => {
     console.log("Like+");
   };
 
+  const sxBgLive = {
+    backgroundColor: theme.palette.secondary.muted,
+  };
+
+  const badge = (
+    <div className={classNames(styles.badge)}>
+      <CircleIcon fontSize="10px" />
+      <p className={classNames(styles.badgeText)}>LIVE</p>
+    </div>
+  );
+
+  const countdown = ({ hours, minutes, seconds }) => (
+    <div className={classNames(styles.countdown)}>
+      {zeroPad(hours)}:{zeroPad(minutes)}:{zeroPad(seconds)}
+    </div>
+  );
+
   return (
     <ThemeProvider theme={theme}>
-      <MuiCard className={classNames(styles.card)} sx={{ maxWidth: 340 }}>
+      <MuiCard
+        className={classNames(styles.card)}
+        sx={{
+          maxWidth: 340,
+          ...(timeLeft && sxBgLive),
+        }}
+      >
         <CardContent>
           <Avatar url={user.avatar.url} verified={user.verified} size="33" />
-          <img src={mediaUrl} className={classNames(styles.media)} />
+          <div className={classNames(styles.mediaContainer)}>
+            {timeLeft && badge}
+            <img src={mediaUrl} className={classNames(styles.media)} />
+            {timeLeft && (
+              <Countdown date={Date.now() + timeLeft} renderer={countdown} />
+            )}
+          </div>
           <div className={classNames(styles.cardText)}>
             <div>
               <Typography className={classNames(styles.title)}>
@@ -69,4 +101,5 @@ Card.propTypes = {
   }),
   price: PropTypes.string,
   currency: PropTypes.string,
+  timeLeft: PropTypes.number,
 };
