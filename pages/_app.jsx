14ac95500bc9ec690e06ccc7a26b6c36bd605544
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-20 15:34:07 +0200

    fix: remove Header component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,7 +6,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
 import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
-import Header from "../src/components/header/Header";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -23,7 +22,6 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Header />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
