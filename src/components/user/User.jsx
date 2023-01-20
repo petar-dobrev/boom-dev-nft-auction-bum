145319a0b0e6596b57a72d713ab97bf81ddccdb7
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-20 16:17:00 +0200

    fix: trying to please VALIDINATOR 3000

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -2,21 +2,19 @@ import classNames from "classnames";
 import Avatar from "../avatar/Avatar";
 import styles from "./User.module.scss";
 
-export default function User(props) {
-  const {
-    name = "",
-    info = "",
-    avatar = "",
-    size = 55,
-    verified = false,
-  } = props;
-
+export default function User({
+  name = "",
+  info = "",
+  avatar = "",
+  size = 55,
+  verified = false,
+}) {
   return (
     <div className={classNames(styles.user)}>
       <Avatar size={size} url={avatar} verified={verified} />
       <div className={classNames(styles.userContent)}>
-          <p className={classNames(styles.name)}>{name}</p>
-          <p className={classNames(styles.info)}>{info}</p>
+        <p className={classNames(styles.name)}>{name}</p>
+        <p className={classNames(styles.info)}>{info}</p>
       </div>
     </div>
   );
