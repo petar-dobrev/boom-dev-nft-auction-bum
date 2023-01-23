c4736fbe4e626dfcb621ecb5bc9c6485502b9b63
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-23 14:46:39 +0200

    fix: class name

--- src/components/productImage/ProductImage.jsx
+++ src/components/productImage/ProductImage.jsx
@@ -2,5 +2,5 @@ import classNames from "classnames";
 import styles from "./ProductImage.module.scss";
 
 export default function ProductImage({ url }) {
-  return <img src={url} className={classNames(styles.productImage)} />;
+  return <img src={url} className={classNames(styles["product-image"])} />;
 }
