bd864561167d0d56d96d439610272240c8793a78
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-25 17:15:08 +0200

    feat: add ProfileUser component

---
+++ src/components/profileUser/ProfileUser.jsx
@@ -0,0 +1,14 @@
+import classNames from "classnames";
+import Avatar from "../avatar/Avatar";
+import { Typography } from "@mui/material";
+import styles from "./ProfileUser.module.scss";
+
+export default function ProfileUser({ name, info, avatar, verified }) {
+  return (
+    <div className={classNames(styles["profile-user"])}>
+      <Avatar url={avatar} verified={verified} size={192} />
+      <Typography variant="h3">{name}</Typography>
+      <Typography variant="body1">{info}</Typography>
+    </div>
+  );
+}
