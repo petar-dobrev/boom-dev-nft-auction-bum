5c501ddde52f231a67926b5f45fee6a46b8176ed
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-27 11:20:28 +0200

    feat: add styling for uppercase and capitalized text

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,10 +2,13 @@ import classNames from "classnames";
 import { Typography } from "@mui/material";
 import styles from "./Hero.module.scss";
 
-export default function Hero({ text }) {
+export default function Hero({ text, capitalize }) {
   return (
     <div className={classNames(styles.hero)}>
-      <Typography variant="h1" className={classNames(styles.text)}>
+      <Typography
+        variant="h1"
+        className={classNames(styles.text, capitalize && styles.capitalize)}
+      >
         {text}
       </Typography>
     </div>
