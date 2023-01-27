e8d6f8c7316a5b374fa88adec71988dbfbd183a8
Petar Dobrev <petar.dobrev92@gmail.com>
2023-01-27 20:43:12 +0200

    feat: add filter functionality

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -7,31 +7,39 @@ import ProfileUser from "../../src/components/profileUser/ProfileUser";
 import ProfileCollection from "../../src/components/profileCollection/ProfileCollection";
 import Footer from "../../src/components/footer/Footer";
 
-import dataProfile from "../../data/profile.json";
-
 export default function Profile() {
   const { query, isReady } = useRouter();
-  const [user, setUser] = useState({});
+  const [profile, setProfile] = useState({});
+  const [profileFilters, setProfileFilters] = useState({});
+
+  const apiUrl = process.env.apiUrl;
+  const id = query.id;
 
   useEffect(() => {
-    setUser(dataProfile || {});
+    if (id != undefined) {
+      fetch(`${apiUrl}/users/${id}`)
+        .then((response) => response.json())
+        .then((data) => setProfile(data.user));
+    }
   }, [isReady]);
 
-  const { username, info, verifed, avatar, nfts } = user || {};
-
-  return Object.keys(user).length == 0 ? (
+  return Object.keys(profile).length == 0 ? (
     <div>Loading</div>
   ) : (
     <>
       <Header />
-      <ProfileHero image={avatar.backgroundUrl} />
+      <ProfileHero image={profile.avatar.backgroundUrl} />
       <ProfileUser
-        name={username}
-        info={info}
-        verified={verifed}
-        avatar={avatar.url}
+        name={profile.username}
+        info={profile.info}
+        verified={profile.verifed}
+        avatar={profile.avatar.url}
       />
-      <ProfileCollection user={user} items={nfts} />
+      <ProfileCollection
+        user={profile}
+        items={profile.nfts}
+        filters={profileFilters}
+      />
       <Footer />
     </>
   );
